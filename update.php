43578dc13554964d9e8f9e64681b4671b97b8ece
annur25 <44317229+annur25@users.noreply.github.com>
2019-08-20 20:02:13 +0700

    Check Dulu

--- update.php
+++ update.php
@@ -1,20 +1,60 @@
-			$sql = "UPDATE  db_produk_forging 
-					SET (nama_produk			='$nama_produk',
-					model 					='$model',
-					part_no_produk_forging			='$part_no_produk_forging',
-					customer				='$customer',
-					status_produk				='$status_produk',
-					sp_mat_pro				='$sp_mat_pro',
-					dim_mat_pro				='$dim_mat_pro',
-					berat_mat				='$berat_mat',
-					berat_pro				='$berat_pro',
-					yield_pro				='$yield_pro',
-					list_type_forging			='$list_type_forging',
-					line_fg_01				='$type_forging_default',
-					line_fg_02				='$type_forging_optional_1',
-					line_fg_03				='$type_forging_optional_2',
-					line_fg_04				='$type_forging_optional_3',
-					line_fg_05				='$type_forging_optional_4',
-					ct_pro 					='$ct_pro',
-					forecast_pro				='$forecast_pro'
-				WHERE 	kode_produk 				='$id_produk_forging'";
+<?php
+	session_start();
+		include 'koneksi.php';
+		$nama_produk = $_POST["nama_produk"];
+		$model = $_POST["model"];
+		$part_no_produk_forging = $_POST["part_no_produk_forging"];
+		$customer = $_POST["customer"];
+		$status_produk = $_POST["status_produk"];
+		$sp_mat_pro = $_POST["sp_mat_pro"];
+		$dim_mat_pro = $_POST["dim_mat_pro"];
+		$berat_mat = $_POST["berat_mat"];
+		$berat_pro = $_POST["berat_pro"];
+		$yield_pro = $_POST["yield_pro"];
+		$list_type_forging = $_POST["list_type_forging"];
+		$type_forging_default = $_POST["line_fg_01"];
+		$type_forging_optional_1 = $_POST["line_fg_02"];
+		$type_forging_optional_2 = $_POST["line_fg_03"];
+		$type_forging_optional_3 = $_POST["line_fg_04"];
+		$type_forging_optional_4 = $_POST["line_fg_05"];
+		$ct_pro = $_POST["ct_pro"];
+		$forecast_pro = $_POST["forecast_pro"];
+		$id_produk_forging = $_POST["kode_produk"];
+
+
+echo $id_produk_forging.$nama_produk.$model.$part_no_produk_forging.$customer;
+
+// 	// query sql
+
+// 	$sql = "UPDATE  db_produk_forging 
+// 			SET 	nama_produk				='$nama_produk',
+// 					model 					='$model',
+// 					part_no_produk_forging	='$part_no_produk_forging',
+// 					customer				='$customer',
+// 					status_produk			='$status_produk',
+// 					sp_mat_pro				='$sp_mat_pro',
+// 					dim_mat_pro				='$dim_mat_pro',
+// 					berat_mat				='$berat_mat',
+// 					berat_pro				='$berat_pro',
+// 					yield_pro				='$yield_pro',
+// 					list_type_forging		='$list_type_forging',
+// 					line_fg_01				='$type_forging_default',
+// 					line_fg_02				='$type_forging_optional_1',
+// 					line_fg_03				='$type_forging_optional_2',
+// 					line_fg_04				='$type_forging_optional_3',
+// 					line_fg_05				='$type_forging_optional_4',
+// 					ct_pro 					='$ct_pro',
+// 					forecast_pro			='$forecast_pro'
+// 			WHERE 	kode_produk 			='$id_produk_forging'";
+
+
+
+// $query = mysqli_query($koneksi, $sql);
+
+// 	if($query){
+// 		echo "Data berhasil dirubah!";
+// 	} else {
+// 		echo "Data gagal dirubah!";
+// 	}
+
+?>
